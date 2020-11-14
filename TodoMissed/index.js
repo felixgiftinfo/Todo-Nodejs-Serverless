3389e765711df60ebb4e53f041efe111c0ac62d6
FG <felixgiftinfo@gmail.com>
2020-11-14 10:59:26 +0100

    Solved issue with local.settings

--- TodoMissed/index.js
+++ TodoMissed/index.js
@@ -1,22 +1,19 @@
 const connectDB = require("../_config/db");
 const TodoModel = require("../_config/models/todo.model");
 
-module.exports = function (context, req) {
+module.exports = async function (context, req) {
   context.log("Get Missed Todos function.");
 
-  console.log("process.env");
-  console.log(process.env);
-  context.res.status(200).json(process.env);
-  // connectDB();
-  // await TodoModel.find({ missed: true })
-  //   .then((x) => {
-  //     if (!x) {
-  //       context.res.status(200).json([]);
-  //     }
-  //     context.res.status(200).json(x.map((x) => x.GetDTO()));
-  //   })
-  //   .catch((err) => {
-  //     context.log(err.message);
-  //     context.res.status(400).json(err.message);
-  //   });
+  connectDB();
+  await TodoModel.find({ missed: true })
+    .then((x) => {
+      if (!x) {
+        context.res.status(200).json([]);
+      }
+      context.res.status(200).json(x.map((x) => x.GetDTO()));
+    })
+    .catch((err) => {
+      context.log(err.message);
+      context.res.status(400).json(err.message);
+    });
 };
