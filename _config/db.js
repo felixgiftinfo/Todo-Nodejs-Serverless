d08cbca580341c5c9acd569b1acb7f3ca9a8fc2e
FG <felixgiftinfo@gmail.com>
2020-11-14 08:43:35 +0100

    Add mode to configuration

--- _config/db.js
+++ _config/db.js
@@ -1,7 +1,5 @@
-
 const mongoose = require("mongoose");
 // ///const {MONGO_URI,MONGO_LOCAL} = require("../config");
-
 
 // mongoose.connect( process.env.MONGO_LOCAL ,{
 //     useNewUrlParser: true,
@@ -11,21 +9,24 @@ const mongoose = require("mongoose");
 // .then(()=>console.log("Mongo DB connected now."))
 // .catch(err=> console.log(err));
 
-
 const connectDB = async () => {
-let MongoURL=process.env.REMOTE_CONNECTION_STRING;
-    try {
-      const conn = await mongoose.connect(MongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      });
-  
-      console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (err) {
-      console.error(err)
-      process.exit(1)
-    }
+  let mongoURL = process.env.REMOTE_CONNECTION_STRING;
+  if (process.env.MODE == "production") {
+    mongoURL = process.env.LOCAL_CONNECTION_STRING;
   }
   
-  module.exports = connectDB
+  try {
+    const conn = await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
