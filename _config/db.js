c76c5ae1c0445360959dd0731c9b61e2bb1a5fca
FG <felixgiftinfo@gmail.com>
2020-11-14 10:42:11 +0100

    Testing getMissedTodo func

--- _config/db.js
+++ _config/db.js
@@ -10,6 +10,9 @@ const mongoose = require("mongoose");
 // .catch(err=> console.log(err));
 
 const connectDB = async () => {
+  console.log('process.env');
+  console.log(process.env);
+
   let mongoURL = process.env.CUSTOMCONNSTR_REMOTE_CONNECTION_STRING;
   if (process.env.CUSTOMCONNSTR_MODE == "development") {
     mongoURL = process.env.CUSTOMCONNSTR_LOCAL_CONNECTION_STRING;
